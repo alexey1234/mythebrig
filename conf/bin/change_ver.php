01c721d91891dd27c07c049783b90375c279f448
Alexey <alexeykr68@gmail.com>
2013-08-30 08:51:30 +0300

    clean hands

--- conf/bin/change_ver.php
+++ conf/bin/change_ver.php
@@ -2,7 +2,7 @@
 <?php
 include ("config.inc");
 if ($config['thebrig']['version'] == 1) { echo "this is first thebrig version"; }
-else {echo "You have new version"; echo "\n"; exit;} exit;}
+else {echo "You have new version"; echo "\n"; exit;} 
 $langfile = file("/usr/local/www/ext/thebrig/lang.inc");
 $version_1 = preg_split ( "/VERSION_NBR, 'v/", $langfile[1]);
 echo "\n";
