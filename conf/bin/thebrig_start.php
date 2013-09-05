cab118505445ca118f908567c28bb83ecb9215d1
Alexey <alexeykr68@gmail.com>
2013-09-05 21:28:43 +0300

    merge branches

--- conf/bin/thebrig_start.php
+++ conf/bin/thebrig_start.php
@@ -17,11 +17,7 @@ if ( count ( $config['thebrig']['content'] ) > 0 ) {
 	if ( !is_file ( "/etc/rc.conf.local" ) ) {
 		// This means we are on embedded
 		write_rcconflocal ();
-		cmd_exec("/etc/rc.d/jail restart",$a_tolog, $a_tolog1);
-		$filelog = $config['thebrig']['rootfolder']."thebrig.log";
-		$handle1 = fopen($filelog, "a+");
-		foreach ($a_tolog1 as $tolog1 ) { fwrite ($handle1, "[".date("Y/m/d H:i:s")."]: TheBrig error!: ".trim($tolog1)."\n" ); }
-		fclose ($handle1);
+		exec ( "/etc/rc.d/jail restart" ) ;
 	}
 }
 ?>
