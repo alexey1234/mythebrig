b480ee5f8a1870cda03b2fc845956324e682656f
Alexey <alexeykr68@gmail.com>
2013-09-13 10:37:54 +0300

    minor update install

--- thebrig_install.sh
+++ thebrig_install.sh
@@ -39,20 +39,9 @@ else
 fi
 
 
-if [ $2 -eq 2 ]; then 
-    # Fetch the testing branch as a zip file
-    echo "Retrieving the testing branch as a zip file"
-    fetch https://github.com/fsbruva/thebrig/archive/working.zip || exerr "ERROR: Could not write to install directory!"
-    mv working.zip master.zip
-elif [ $2 -eq 3 ]; then
-	echo "Retrieving the alexey's branch as a zip file"
-	fetch https://github.com/fsbruva/thebrig/archive/alexey.zip || exerr "ERROR: Could not write to install directory!"
-	mv alexey.zip master.zip
-else
-	# Fetch the master branch as a zip file
-	echo "Retrieving the most recent version of TheBrig"
-	fetch https://github.com/fsbruva/thebrig/archive/master.zip || exerr "ERROR: Could not write to install directory!"
-fi
+# Fetch the master branch as a zip file
+echo "Retrieving the most recent version of TheBrig"
+fetch https://github.com/alexey1234/mythebrig/archive/master.zip || exerr "ERROR: Could not write to install directory!"
 
 
 # Extract the files we want, stripping the leading directory, and exclude
